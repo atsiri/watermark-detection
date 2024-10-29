1c16f88f247d2dc8b313b6c7a6381513b16b516e
rofiqoh <55867056+atsiri@users.noreply.github.com>
2024-10-29 13:13:59 +0700

    update confusion matrix

--- watermarkmodel/model/metrics.py
+++ watermarkmodel/model/metrics.py
@@ -6,4 +6,5 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 def plot_confusion_matrix(x: np.ndarray, y: np.ndarray):
     cm = confusion_matrix(x, y)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['non_watermark', 'watermark'])
+    disp.savefig('confmatrix.png')
     return disp.plot()
